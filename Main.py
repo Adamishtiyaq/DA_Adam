ec56b233ff766ed09db42921c22d75482e54f769
Adamishtiyaq <adamishtiyaq99@gmail.com>
2020-02-28 16:18:02 +0800

    DATA FILE

---
+++ Main.py
@@ -0,0 +1,17 @@
+class DA:
+    def __init__(self, sno, regions, datafile):
+        self.sno = sno
+        self.regions = regions
+        self.datafile = data.xlsx
+
+    dict = {"region": ["asia"],
+            "period"["1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987"]}
+
+    # reading excel data
+    def read_data():
+        pd.read_excel()
+        print(datafile)
+
+
+
+
