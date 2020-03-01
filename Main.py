4d0f4471d51c7d2b59cd46db541b0509404c51ff
Adamishtiyaq <adamishtiyaq99@gmail.com>
2020-03-01 16:50:01 +0800

    DATA FILE

--- Main.py
+++ Main.py
@@ -12,8 +12,9 @@ class ASP:
 
         file_data = 'Worksheet in D  Lesson 2019 Applied Sriptong Using Python Python Elective (IT49450) - Project_14Mar19.csv'
 
-        df = pd.read_csv(file_data, delimiter='\t', names = ['Brunei Darussalam', 'Indonesia', 'Malaysia'])
+        df = pd.read_csv(file_data, delimiter='\t', header=None, names = ['Brunei Darussalam', 'Indonesia', 'Malaysia'])
         df = df.isnull()
         print(df.head(3))
+        print('1978 - 1987')
 
     read_csvdata()
